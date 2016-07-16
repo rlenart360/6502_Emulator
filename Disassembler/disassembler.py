c194bbb9ecf1b4078106c18fd5e999fc7e3da8e5
Ryan Lenart <rlenart360@gmail.com>
2016-07-16 14:14:02 -0700

    Update disassembler.py

--- Disassembler/disassembler.py
+++ Disassembler/disassembler.py
@@ -2,17 +2,64 @@ class Disassembler(object):
     def __init__(self):
         pass
 
-    def process_hex(self, rom_hex: int):
+    def process_hex(self, rom_hex):
         print(rom_hex)
 
-        for i in range(len(rom_hex)):
-            print(i)
+        i = 0
+        while i < (len(rom_hex)):
+
             current_byte = rom_hex[i]
 
-            # LSR (Logical Shift Right), Length 3 bytes
+            # LSR (Logical Shift Right), MODE: Absolute, LENGTH: 3 bytes
             if (rom_hex[i] == 78):
-                print('LSR '+'$'+hex(rom_hex[i+2])[2:]+hex(rom_hex[i+1])[2:])
+                print('LSR ' + '$' + hex(rom_hex[i + 2])[2:] + hex(rom_hex[i + 1])[2:])
+                i += 2
+
+            # ORA (Bitwise OR with Accumulator), MODE: Indirect, X, LENGTH: 2 bytes
+            elif (rom_hex[i] == 1):
+                print('ORA ' +'($' +hex(rom_hex[i+1])[2:] + ',X)')
+                i+=1
+
+            # SEI (Set Interrupt), Processor Status Intruction
+            elif (rom_hex[i] == 120):
+                print('SEI')
+
+            # CLD (Clear Decimal), Processor Status Intruction
+            elif (rom_hex[i] == 216):
+                print('CLD')
+
+            # ADC (Add with Carry)
+            elif (rom_hex[i] == 105):
+                print('ADC '+'#$'+hex(rom_hex[i+1])[2:])
+                i+=1
+
+            # ADC
+            elif (rom_hex[i] == 101):
+                print('ADC '+'$'+hex(rom_hex[i+1])[2:])
+                i+=1
+
+            elif (rom_hex[i] == 117):
+                print('ADC '+'$'+hex(rom_hex[i+1])[2:]+',X')
+                i+=1
+
+            elif (rom_hex[i] == 109):
+                print('ADC '+'$'+hex(rom_hex[i+2])[2:]+hex(rom_hex[i+1])[2:])
                 i+=2
-                print(i)
 
+            elif (rom_hex[i] == 125):
+                print('ADC '+'$'+hex(rom_hex[i+2])[2:]+hex(rom_hex[i+1])[2:]+',X')
+                i+=2
 
+            elif (rom_hex[i] == 121):
+                print('ADC ' + '$' + hex(rom_hex[i + 2])[2:] + hex(rom_hex[i + 1])[2:] + ',Y')
+                i+=2
+
+            elif (rom_hex[i] == 97):
+                print('ADC ' + '($' + hex(rom_hex[i + 1])[2:] + ',X)')
+                i+=1
+
+            elif (rom_hex[i] == 113):
+                print('ADC ' + '($' + hex(rom_hex[i + 1])[2:] + '),Y')
+                i+=1
+
+            i += 1
