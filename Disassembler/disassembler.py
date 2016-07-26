22d4cdc0d3eff75f2f87a339cae89990ae212810
Ryan Lenart <rlenart360@gmail.com>
2016-07-26 13:31:35 -0700

    Update Disassembler
    Adding more instructions.

--- Disassembler/disassembler.py
+++ Disassembler/disassembler.py
@@ -6,8 +6,7 @@ class Disassembler(object):
 
     def process_hex(self, rom_hex):
         print(rom_hex)
-        print_bytes(10)
-
+        
         # Loop that will process the ROM
         i = 0
         while i < (len(rom_hex)):
@@ -141,7 +140,156 @@ class Disassembler(object):
             elif (rom_hex[i] == 80):
             	print('BVC ' + '$' + print_bytes(rom_hex[i + 1]))
             	i += 1
-
+            	
+            # BVS (Branch on oVerflow Set)
+            elif (rom_hex[i] == 112):
+            	print('BVS ' + '$' + print_bytes(rom_hex[i + 1]))
+            	i += 1
+            	
+            # BCC (Branch on Carry Clear)
+            elif (rom_hex[i] == 144):
+            	print('BCC ' + '$' + print_bytes(rom_hex[i + 1]))
+            	i += 1
+            	
+            # BCS (Branch on Carry Set)
+            elif (rom_hex[i] == 176):
+            	print('BCS ' + '$' + print_bytes(rom_hex[i + 1]))
+            	i += 1
+            	
+            # BNE (Branch on Not Equal)
+            elif (rom_hex[i] == 208):
+            	print('BNE ' + '$' + print_bytes(rom_hex[i + 1]))
+            	i += 1
+            	
+            # BEQ (Branch on EQual)
+            elif (rom_hex[i] == 240):
+            	print('BEQ ' + '$' + print_bytes(rom_hex[i + 1]))
+            	i += 1
+            
+            # BRK (Break)
+            elif (rom_hex[i] == 0):
+            	print('BRK')
+            
+            # CMP (Compare Accumulator)
+            elif (rom_hex[i] == 201):
+            	print('CMP ' + '#$' + print_bytes(rom_hex[i + 1]))
+            	i += 1
+            	
+            elif (rom_hex[i] == 197):
+            	print('CMP ' + '$' + print_bytes(rom_hex[i + 1]))
+            	i += 1
+            	
+            elif (rom_hex[i] == 213):
+            	print('CMP ' + '$' + print_bytes(rom_hex[i + 1]) + ',X')
+            	i += 1
+            	
+            elif (rom_hex[i] == 205):
+            	print('CMP ' + '$' + print_bytes(rom_hex[i + 2]) + print_bytes(rom_hex[i + 1]))
+            	i += 2
+            	
+            elif (rom_hex[i] == 221):
+            	print('CMP ' + '$' + print_bytes(rom_hex[i + 2]) + print_bytes(rom_hex[i + 1]) + ',X')
+            	i += 2
+            	
+            elif (rom_hex[i] == 217):
+            	print('CMP ' + '$' + print_bytes(rom_hex[i + 2]) + print_bytes(rom_hex[i + 1]) + ',Y')
+            	i += 2
+            	
+            elif (rom_hex[i] == 193):
+            	print('CMP ' + '($' + print_bytes(rom_hex[i + 1]) + ',X)')
+            	i += 1
+            	
+            elif (rom_hex[i] == 209):
+            	print('CMP ' + '($' + print_bytes(rom_hex[i + 1]) + '),Y')
+            	i += 1
+            
+            # CPX (Compare X Register)
+            elif (rom_hex[i] == 224):
+            	print('CPX ' + '#$' + print_bytes(rom_hex[i + 1]))
+            	i += 1
+            	
+            elif (rom_hex[i] == 228):
+            	print('CPX ' + '$' + print_bytes(rom_hex[i + 1]))
+            	i += 1
+            	
+            elif (rom_hex[i] == 236):
+            	print('CPX ' + '$' + print_bytes(rom_hex[i + 2]) + print_bytes(rom_hex[i + 1]))
+            	i += 2
+            
+            # CPY (Compare Y Register)
+            elif (rom_hex[i] == 192):
+            	print('CPY ' + '#$' + print_bytes(rom_hex[i + 1]))
+            	i += 1
+            	
+            elif (rom_hex[i] == 196):
+            	print('CPY ' + '$' + print_bytes(rom_hex[i + 1]))
+            	i += 1
+            	
+            elif (rom_hex[i] == 204):
+            	print('CPY ' + '$' + print_bytes(rom_hex[i + 2]) + print_bytes(rom_hex[i + 1]))
+            	i += 2
+            
+            # DEC (Decrement Memory)
+            
+            # EOR (Bitwise Exclusive OR)
+            
+            # CLC (Clear Carry)
+            # SEC (Set Carry)
+            # CLI (Clear Interrupt)
+            # SEI (Set Interrupt)
+            # CLV (Clear Overflow)
+            # CLD (Clear Decimal)
+            # SED (Set Decimal)
+            
+            # INC (Increment Memory)
+            
+            # JMP (Jump)
+            
+            # JSR (Jump to Subroutine)
+            
+            # LDA (Load Accumulator)
+            
+            # LDX (Load X Register)
+            
+            # LDY (Load Y Register)
+            
+            # LSR (Logical Shift Right)
+            
+            # NOP (No Operation)
+            
+            # ORA (Bitwise OR with Accumulator)
+            
+            # TAX (Transfer A to X)
+            # TXA (Transfer X to A)
+            # DEX (Decrement X)
+            # INX (Increment X)
+            # TAY (Transfer A to Y)
+            # TYA (Transfer Y to A)
+            # DEY (Decrement Y)
+            # INY (Increment Y)
+            
+            # ROL (Rotate Left)
+            
+            # ROR (Rotate Right)
+            
+            # RTI (Return from Interrupt)
+            
+            # RTS (Return from Subroutine)
+            
+            # SBC (Subtract with Carry)
+            
+            # STA (Store Accumulator)
+            
+            # TXS (Transfer X to Stack Ptr)
+            # TSX (Transfer Stack Ptr to X)
+            # PHA (Push Accumulator)
+            # PLA (Pull Accumulator)
+            # PHP (Push Processor Status)
+            # PLP (Pull Processor Status)
+            
+            # STX (Store X Register)
+            
+            # STY (Store Y Register)
             # TODO: Keep working on the Disassembler
 
             i += 1
