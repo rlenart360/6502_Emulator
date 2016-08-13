e65a659492f0fd47611c18a364e035d282498072
Ryan Lenart <rlenart360@gmail.com>
2016-08-12 21:34:26 -0700

    Add graphics.py
    I think I can make this work..

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import argparse
+from tkinter import *
+from tkinter import filedialog
 from cpu import CPU
 
 
@@ -16,10 +18,22 @@ def main():
         
     # TEST INSTRUCTIONS
     test = [117, 0]
-
+    
     # Create the CPU
     cpu = CPU()
     cpu.process_instruction(test)
+    print(cpu.A)
+    
+    root = Tk()
+    frame = Frame(root, height = 532, width = 532)
+    frame.pack()
+    
+    screen = Canvas(frame, bg = "black")
+    screen.pack()
+    screen.place(x =10 , y =10 , height = 512, width = 512)
+    screen.create_rectangle(0, 0, 16, 16, fill='red')
+
+    root.mainloop()
 
 
 if __name__ == '__main__':
